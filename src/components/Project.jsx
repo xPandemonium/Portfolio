bd14f78029d2972e540d116814a3c4d2a4ae1ac0
Pandemonium <jonathancorreaiglesias@gmail.com>
2025-03-02 01:47:19 -0600

    Made the all the pages and text more responsive

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,14 +2,14 @@ import { Styles } from '../Styles/Styles';
 
 export default function Projects({ name , image , description, link }) {
   return (
-    <div className={`${Styles.paragraphContainer}`}>
+    <div className={`${Styles.paragraphContainer} sm:col-span-1 col-span-2`}>
       <div>
-        <figure className="relative transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+        <figure className="relative transition-all duration-300 cursor-pointer sm:filter sm:grayscale sm:hover:grayscale-0">
           <a href={link} target="_blank">
             <img className="rounded-lg" src={image} alt="image description"/>
           </a>
         </figure>
-        <h2>{name}</h2>
+        <h2 className='text-center sm:text-2xl text-xl font-semibold underline'>{name}</h2>
         <p>{description}</p>
       </div>
     </div>    
